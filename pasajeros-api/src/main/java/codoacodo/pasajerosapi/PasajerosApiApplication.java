e5767d0d64d16010094636ea0abb01aa65d97d91
Veronica <dv.veronica2075@gmail.com>
2024-03-09 23:48:43 -0300

    first commit

---
+++ pasajeros-api/src/main/java/codoacodo/pasajerosapi/PasajerosApiApplication.java
@@ -0,0 +1,13 @@
+package codoacodo.pasajerosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasajerosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasajerosApiApplication.class, args);
+	}
+
+}
---
+++ pasajeros-api/src/test/java/codoacodo/pasajerosapi/PasajerosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package codoacodo.pasajerosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasajerosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
